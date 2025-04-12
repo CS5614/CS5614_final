e3493ebad32f47bd5d589c1dfecda734ad3ee29a
css186 <cssrm31@gmail.com>
2025-04-12 01:45:42 -0400

    Modify methodology of clustering

---
+++ utils/db_engine.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+class DBEngine:
+    def __init__(self):
+        load_dotenv()
+        self.db_host = os.getenv("DB_HOST")
+        self.db_name = os.getenv("DB_NAME")
+        self.db_user = os.getenv("DB_USER")
+        self.db_password = os.getenv("DB_PASSWORD")
+
+        if not all([self.db_host, self.db_name, self.db_user, self.db_password]):
+            logging.INFO("Database connection parameters are not set in the environment variables.")
+            raise ValueError("Database connection parameters are not set in the environment variables.")
+        self._engine = None
+
+    def get_engine(self):
+        if not self._engine:
+            self._engine = create_engine(
+                f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}/{self.db_name}"
+            )
+            logging.info("Database engine created successfully.")
+        return self._engine
