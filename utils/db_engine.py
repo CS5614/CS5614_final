9e7ef6e7c4532b4d9657a370950ffd603c71a47b
css186 <cssrm31@gmail.com>
2025-04-14 04:18:16 -0400

    Complete walkability index parsing and storing

--- utils/db_engine.py
+++ utils/db_engine.py
@@ -12,8 +12,9 @@ class DBEngine:
         self.db_name = os.getenv("DB_NAME")
         self.db_user = os.getenv("DB_USER")
         self.db_password = os.getenv("DB_PASSWORD")
+        self.db_port = os.getenv("DB_PORT", "5432")
 
-        if not all([self.db_host, self.db_name, self.db_user, self.db_password]):
+        if not all([self.db_host, self.db_name, self.db_user, self.db_password, self.db_port]):
             logging.INFO("Database connection parameters are not set in the environment variables.")
             raise ValueError("Database connection parameters are not set in the environment variables.")
         self._engine = None
@@ -21,7 +22,7 @@ class DBEngine:
     def get_engine(self):
         if not self._engine:
             self._engine = create_engine(
-                f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}/{self.db_name}"
+                f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
             )
             logging.info("Database engine created successfully.")
         return self._engine
